b3707741d59b060a6dacea3c8399bdfcf61cb44c
Max Zakharzhevskiy <maxzz@msn.com>
2023-02-06 15:54:56 -0800

    add vite-plugin-electron

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import electron from 'vite-plugin-electron';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        electron({
+            entry: 'electron/main.ts'
+        }),
+    ],
+});
